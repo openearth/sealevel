9d622ede5798af30404bd6cd319a815c74f5454c
Fedor Baart <fedor.baart@deltares.nl>
2022-10-16 15:46:45 +0200

    merge changes from #23

--- slr/slr/wind.py
+++ slr/slr/wind.py
@@ -7,17 +7,38 @@ import pandas as pd
 import slr
 
 
-def make_wind_df(lat_i=53, lon_i=3, local=True):
+def make_wind_df(lat_i=53, lon_i=3, product="NCEP1", local=True):
     """create a dataset for wind, for 1 latitude/longitude"""
-    # the following url's are not available during a government shutdown
-    u_file = "http://www.esrl.noaa.gov/psd/thredds/dodsC/Datasets/ncep.reanalysis.derived/surface_gauss/uwnd.10m.mon.mean.nc"
-    v_file = "http://www.esrl.noaa.gov/psd/thredds/dodsC/Datasets/ncep.reanalysis.derived/surface_gauss/vwnd.10m.mon.mean.nc"
 
     src_dir = slr.get_src_dir()
+    if product == "NCEP1":
+        # the following url's are not available during a government shutdown
+        u_file = "http://www.esrl.noaa.gov/psd/thredds/dodsC/Datasets/ncep.reanalysis.derived/surface_gauss/uwnd.10m.mon.mean.nc"
+        v_file = "http://www.esrl.noaa.gov/psd/thredds/dodsC/Datasets/ncep.reanalysis.derived/surface_gauss/vwnd.10m.mon.mean.nc"
 
-    if local:
-        u_file = pathlib.Path(src_dir / "data/noaa/uwnd.10m.mon.mean.nc").expanduser()
-        v_file = pathlib.Path(src_dir / "data/noaa/vwnd.10m.mon.mean.nc").expanduser()
+        if local:
+            u_file = pathlib.Path(
+                src_dir / "data/noaa/uwnd.10m.mon.mean.nc"
+            ).expanduser()
+            v_file = pathlib.Path(
+                src_dir / "data/noaa/vwnd.10m.mon.mean.nc"
+            ).expanduser()
+
+    elif product == "20CR":
+        if local:
+            u_file = pathlib.Path(
+                src_dir / "data/noaa/20cr.uwnd.10m.mon.mean.nc"
+            ).expanduser()
+            v_file = pathlib.Path(
+                src_dir / "data/noaa/20cr.vwnd.10m.mon.mean.nc"
+            ).expanduser()
+        else:
+            print(
+                f"ERROR: The reanalysis product {product} first needs to be downloaded"
+            )
+
+    else:
+        print(f"Reanalysis product not available: {product}")
 
     # open the 2 files
     ds_u = netCDF4.Dataset(u_file)
@@ -74,3 +95,37 @@ def make_wind_df(lat_i=53, lon_i=3, local=True):
 
     # return it
     return wind_df
+
+
+def make_annual_wind_df(wind_df):
+    """Compute annual averaged wind dataset from monthly wind dataset"""
+
+    # label set to xxxx-01-01 of the current year
+    annual_wind_df = wind_df.groupby(pd.Grouper(freq="1Y", label="left")).mean()
+    annual_wind_df.index = annual_wind_df.index + datetime.timedelta(days=1)
+    annual_wind_df["speed"] = np.sqrt(
+        annual_wind_df["u"] ** 2 + annual_wind_df["v"] ** 2
+    )
+    annual_wind_df["direction"] = np.mod(
+        np.angle(annual_wind_df["u"] + annual_wind_df["v"] * 1j), 2 * np.pi
+    )
+
+    return annual_wind_df
+
+
+def combine_linear_scaling(df1, df2):
+    """Combine two reanalysis products with a linear scaling using the most
+    recent one as reference"""
+
+    date_s = df2.index[0]
+    date_e = df1.index[-1]
+
+    df1_cor = df1.copy()
+
+    df1_cor = df1 - df1.loc[date_s:date_e].mean()
+    df1_cor = df1_cor * df2.loc[date_s:date_e].std() / df1_cor.loc[date_s:date_e].std()
+    df1_cor = df1_cor + df2.loc[date_s:date_e].mean()
+
+    combined_df = pd.concat([df1_cor.loc[:date_s][:-1], df2.loc[date_s:]])
+
+    return combined_df
